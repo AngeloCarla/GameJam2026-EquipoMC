e69d93bec5632562cf3ff2da58c08fd45a139cf2
AngeloCarla <carladalilaa@gmail.com>
2026-02-03 17:12:39 -0300

    Obstaculos y daño
    Se agrega script de daño y se arregla la zona de gas

--- Assets/Scripts/GasZone/area_2d.gd
+++ Assets/Scripts/GasZone/area_2d.gd
@@ -1,14 +1,19 @@
 extends Area2D
 @export var drainMultiplier = 4.0
 
+func _ready():
+	# CONECTA LOS SIGNALS (OBLIGATORIO)
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	
 func _on_body_entered(body: Node2D) -> void:
 	# Llama el metodo en player 
-	if body.has_method("SetOxygenDrainMultiplier"):
-		# Cada que esta en la zona baja x4
-		body.SetOxygenDrainMultiplier(drainMultiplier)
-		#print("Golpeado")
+	if body.is_in_group("player") or body.get_class() == "CharacterBody2D":
+		if body.has_method("SetOxygenDrainMultiplier"):
+			body.SetOxygenDrainMultiplier(drainMultiplier)
+			print("Golpeado")
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.has_method("SetOxygenDrainMultiplier"):
 		body.SetOxygenDrainMultiplier(1.0) # Vuelve al normal
-		#print("Saliste")
+		print("Saliste")
